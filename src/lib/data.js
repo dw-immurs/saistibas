d938788b0bce48440f82354c0edb115f47bbcf7a
Gustavs Švalbe <gustavssvalbe@gmail.com>
2025-12-15 10:11:32 +0200

    Update

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,178 +1,178 @@
 export const CONNECTION_GAMES = [
   [
-{
-  category: "ĪPAŠĪBAS VĀRDI SIEVIEŠU DZIMTĒ",
-  words: ["KAILA", "GARDA", "BRANGA", "NERETA"],
-  difficulty: 1,
-},
-{
-  category: "KONDITOREJAS/BEĶEREJAS RĪGĀ",
-  words: ["KŪRE", "DRUSTI", "MĪKLA", "SALA"],
-  difficulty: 2,
-},
-{
-  category: "UPES AR DZĪVNIEKIEM NOSAUKUMĀ",
-  words: ["STIRNA", "VĒRŠĀDA", "NEVĒŽA", "BRIEDE"],
-  difficulty: 3,
-},
-{
-  category: "VĀRDI NEGATĪVU EMOCIJU PAUŠANAI",
-  words: ["DIEVIŅŠ", "SVIESTS", "NELAIME", "NERAŽA"],
-  difficulty: 4,
-},
-  ],
-  [
-{
-  category: "KRŪMI",
-  words: ["CERIŅI", "KORINTE", "KAMĒLIJA", "IEVA"],
-  difficulty: 1,
-},
-{
-  category: "PALINDROMI",
-  words: ["ANNA", "IKI", "ALLA", "SALAS"],
-  difficulty: 2,
-},
-{
-  category: "SĀKAS AR NOTI",
-  words: ["SOLISTS", "RENEGĀTS", "MIO", "DODO"],
-  difficulty: 3,
-},
-{
-  category: "FINANŠU MINISTRU UZVĀRDI",
-  words: ["VILKS", "OSIS", "ZĪLE", "VILKS"],
-  difficulty: 4,
-},
-  ],
-  [
-{
-  category: "CEĻU SATIKSME",
-  words: ["TILTS", "PĀRVADS", "APVEDCEĻŠ", "TRANZĪTS"],
-  difficulty: 1,
-},
-{
-  category: "VELOSIPĒDA RITEŅA ELEMENTI",
-  words: ["RUMBA", "SPIEĶI", "APLOKS", "RIEPA"],
-  difficulty: 2,
-},
-{
-  category: "IELAS DZIESMU NOSAUKUMOS",
-  words: ["PELDU", "BRĪVĪBAS", "ALISES", "MIERA"],
-  difficulty: 3,
-},
-{
-  category: "SATUR ARHITEKTŪRAS TERMINU (ARKA, ĀTRIJS, MARGA, JOMS)",
-  words: ["MARKA", "NĀTRIJS", "MARGARITAS", "APJOMS"],
-  difficulty: 4,
-},
+    {
+      category: "ĪPAŠĪBAS VĀRDI SIEVIEŠU DZIMTĒ",
+      words: ["KAILA", "GARDA", "BRANGA", "NERETA"],
+      difficulty: 1,
+    },
+    {
+      category: "KONDITOREJAS/BEĶEREJAS RĪGĀ",
+      words: ["KŪRE", "DRUSTI", "MĪKLA", "SALA"],
+      difficulty: 2,
+    },
+    {
+      category: "UPES AR DZĪVNIEKIEM NOSAUKUMĀ",
+      words: ["STIRNA", "VĒRŠĀDA", "NEVĒŽA", "BRIEDE"],
+      difficulty: 3,
+    },
+    {
+      category: "VĀRDI NEGATĪVU EMOCIJU PAUŠANAI",
+      words: ["DIEVIŅŠ", "SVIESTS", "NELAIME", "NERAŽA"],
+      difficulty: 4,
+    },
   ],
   [
     {
-  category: "TENISA ELEMENTI",
-  words: ["TROSE", "TĪKLS", "KORTS", "BUMBIŅA"],
-  difficulty: 1,
-},
-{
-  category: "PĀRSLAS",
-  words: ["SNIEGS", "PAPĪRA", "AUZAS", "BROKASTIS"],
-  difficulty: 3,
-},
-{
-  category: "PIRMAIS VĀRDS ERUDĪCIJAS SPĒĻU NOSAUKUMĀ",
-  words: ["ZINI", "GRIBI", "PRĀTA", "GUDRS"],
-  difficulty: 2,
-},
-{
-  category: "SVĀRKU VEIDI",
-  words: ["MINI", "ĀDAS", "ZVANS", "VOLĀNS"],
-  difficulty: 4,
-},
+      category: "KRŪMI",
+      words: ["CERIŅI", "KORINTE", "KAMĒLIJA", "IEVA"],
+      difficulty: 1,
+    },
+    {
+      category: "PALINDROMI",
+      words: ["ANNA", "IKI", "ALLA", "SALAS"],
+      difficulty: 2,
+    },
+    {
+      category: "SĀKAS AR NOTI",
+      words: ["SOLISTS", "RENEGĀTS", "MIO", "DODO"],
+      difficulty: 3,
+    },
+    {
+      category: "FINANŠU MINISTRU UZVĀRDI",
+      words: ["VILKS", "OSIS", "ZĪLE", "VILKS"],
+      difficulty: 4,
+    },
   ],
   [
-  {
-  category: "TAUTUMEITAS",
-  words: ["AURĒLIJA", "LAURA", "KATE", "GABRIĒLA"],
-  difficulty: 1,
-},
-{
-  category: "OSCARS LABĀKĀS AKTRISES NO 2020. GADA",
-  words: ["EMMA", "DŽESIKA", "MIŠELA", "MAIKIJA"],
-  difficulty: 2,
-},
-{
-  category: "IELAS RĪGAS CENTRĀ",
-  words: ["MARIJA", "MARTA", "ELIZABETE", "ANNA"],
-  difficulty: 3,
-},
-{
-  category: "DŽILINDŽERA SIEVAS",
-  words: ["INGA", "BINNIJA", "DACE", "MAIJA"],
-  difficulty: 4,
-},
+    {
+      category: "CEĻU SATIKSME",
+      words: ["TILTS", "PĀRVADS", "APVEDCEĻŠ", "TRANZĪTS"],
+      difficulty: 1,
+    },
+    {
+      category: "VELOSIPĒDA RITEŅA ELEMENTI",
+      words: ["RUMBA", "SPIEĶI", "APLOKS", "RIEPA"],
+      difficulty: 2,
+    },
+    {
+      category: "IELAS DZIESMU NOSAUKUMOS",
+      words: ["PELDU", "BRĪVĪBAS", "ALISES", "MIERA"],
+      difficulty: 3,
+    },
+    {
+      category: "SATUR ARHITEKTŪRAS TERMINU (ARKA, ĀTRIJS, MARGA, JOMS)",
+      words: ["MARKA", "NĀTRIJS", "MARGARITAS", "APJOMS"],
+      difficulty: 4,
+    },
   ],
   [
-      {
-    category: "MĒRVIENĪBAS DAUDZSKAITLĪ",
-    words: ["PĒDAS", "KUBI", "ARKLI", "GRĀDI"],
-    difficulty: 1,
-  },
-  {
-    category: "“IELAS” LĪMEŅA ADRESĀCIJAS SUGAS VĀRDI",
-    words: ["STŪRIS", "SĒTA", "LĪNIJA", "LAUKUMS"],
-    difficulty: 4,
-  },
-  {
-    category: "SILA …",
-    words: ["CIEMS", "STRAZDS", "KROGS", "VIRSIS"],
-    difficulty: 3,
-  },
-  {
-    category: "ĶERMEŅA DAĻAS TEICIENOS PAR MELIEM VAI KRĀPŠANU",
-    words: ["KĀJAS", "DEGUNS", "KĀJAS", "ACIS"],
-    difficulty: 2,
-  },
+    {
+      category: "TENISA ELEMENTI",
+      words: ["TROSE", "TĪKLS", "KORTS", "BUMBIŅA"],
+      difficulty: 1,
+    },
+    {
+      category: "PĀRSLAS",
+      words: ["SNIEGS", "PAPĪRA", "AUZAS", "BROKASTIS"],
+      difficulty: 3,
+    },
+    {
+      category: "PIRMAIS VĀRDS ERUDĪCIJAS SPĒĻU NOSAUKUMĀ",
+      words: ["ZINI", "GRIBI", "PRĀTA", "GUDRS"],
+      difficulty: 2,
+    },
+    {
+      category: "SVĀRKU VEIDI",
+      words: ["MINI", "ĀDAS", "ZVANS", "VOLĀNS"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "TAUTUMEITAS",
+      words: ["AURĒLIJA", "LAURA", "KATE", "GABRIĒLA"],
+      difficulty: 1,
+    },
+    {
+      category: "OSCARS LABĀKĀS AKTRISES NO 2020. GADA",
+      words: ["EMMA", "DŽESIKA", "MIŠELA", "MAIKIJA"],
+      difficulty: 2,
+    },
+    {
+      category: "IELAS RĪGAS CENTRĀ",
+      words: ["MARIJA", "MARTA", "ELIZABETE", "ANNA"],
+      difficulty: 3,
+    },
+    {
+      category: "DŽILINDŽERA SIEVAS",
+      words: ["INGA", "BINNIJA", "DACE", "MAIJA"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "MĒRVIENĪBAS DAUDZSKAITLĪ",
+      words: ["PĒDAS", "KUBI", "ARKLI", "GRĀDI"],
+      difficulty: 1,
+    },
+    {
+      category: "“IELAS” LĪMEŅA ADRESĀCIJAS SUGAS VĀRDI",
+      words: ["STŪRIS", "SĒTA", "LĪNIJA", "LAUKUMS"],
+      difficulty: 4,
+    },
+    {
+      category: "SILA …",
+      words: ["CIEMS", "STRAZDS", "KROGS", "VIRSIS"],
+      difficulty: 3,
+    },
+    {
+      category: "ĶERMEŅA DAĻAS TEICIENOS PAR MELIEM VAI KRĀPŠANU",
+      words: ["KĀJAS", "DEGUNS", "KĀJAS", "ACIS"],
+      difficulty: 2,
+    },
+  ],
+  [
+    {
+      category: "SALDAIS …",
+      words: ["PĀRĪTIS", "KRĒJUMS", "ĒDIENS", "VĪNS"],
+      difficulty: 1,
+    },
+    {
+      category: "MATEMĀTIKAS PALĪGLĪDZEKĻI",
+      words: ["PIRKSTI", "KAULIŅI", "KALKULATORS", "PAPĪRS"],
+      difficulty: 2,
+    },
+    {
+      category: "DIVDABJI",
+      words: ["SVIESTS", "PROTAMS", "METAMS", "ZINOŠI"],
+      difficulty: 3,
+    },
+    {
+      category: "MĒRVIENĪBAS",
+      words: ["PĒDAS", "ARKLI", "KUBI", "PINTES"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "NAUDAS APZĪMĒJUMI ĢENITĪVĀ",
+      words: ["PIĶA", "KĀPOSTA", "SKANOŠĀ", "PESO"],
+      difficulty: 1,
+    },
+    {
+      category: "… STACIJAS",
+      words: ["VILCES", "BĀZES", "PASTA", "DZELZCEĻA"],
+      difficulty: 2,
+    },
+    {
+      category: "… PILIENS",
+      words: ["DARVAS", "APRĪĻA", "MEDUS", "PĒDĒJAIS"],
+      difficulty: 3,
+    },
+    {
+      category: "PĒDĒJAIS VĀRDS KULDĪGAS GRUPU NOSAUKUMOS ĢENITĪVĀ",
+      words: ["KVARTETA", "ZĒVELES", "PUTNU", "VAGONA"],
+      difficulty: 4,
+    },
   ]
-  [
-  {
-    category: "SALDAIS …",
-    words: ["PĀRĪTIS", "KRĒJUMS", "ĒDIENS", "VĪNS"],
-    difficulty: 1,
-  },
-  {
-    category: "MATEMĀTIKAS PALĪGLĪDZEKĻI",
-    words: ["PIRKSTI", "KAULIŅI", "KALKULATORS", "PAPĪRS"],
-    difficulty: 2,
-  },
-  {
-    category: "DIVDABJI",
-    words: ["SVIESTS", "PROTAMS", "METAMS", "ZINOŠI"],
-    difficulty: 3,
-  },
-  {
-    category: "MĒRVIENĪBAS",
-    words: ["PĒDAS", "ARKLI", "KUBI", "PINTES"],
-    difficulty: 4,
-  },
-]
-[
-  {
-    category: "NAUDAS APZĪMĒJUMI ĢENITĪVĀ",
-    words: ["PIĶA", "KĀPOSTA", "SKANOŠĀ", "PESO"],
-    difficulty: 1,
-  },
-  {
-    category: "… STACIJAS",
-    words: ["VILCES", "BĀZES", "PASTA", "DZELZCEĻA"],
-    difficulty: 2,
-  },
-  {
-    category: "… PILIENS",
-    words: ["DARVAS", "APRĪĻA", "MEDUS", "PĒDĒJAIS"],
-    difficulty: 3,
-  },
-  {
-    category: "PĒDĒJAIS VĀRDS KULDĪGAS GRUPU NOSAUKUMOS ĢENITĪVĀ",
-    words: ["KVARTETA", "ZĒVELES", "PUTNU", "VAGONA"],
-    difficulty: 4,
-  },
-]
 ];
