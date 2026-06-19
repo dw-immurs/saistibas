7281939db36f9e1948c72033ed744bac78bbdf84
dw-immurs <davis.immurs@gmail.com>
2026-06-19 20:15:15 +0300

    add special game data file

---
+++ src/lib/data2.js
@@ -0,0 +1,24 @@
+export const CONNECTION_GAMES = [
+  [
+    {
+      category: "ĪPAŠĪBAS VĀRDI SIEVIEŠU DZIMTĒ",
+      words: ["KAILA", "GARDA", "BRANGA", "SKAISTA"],
+      difficulty: 1,
+    },
+    {
+      category: "KONDITOREJAS/BEĶEREJAS RĪGĀ",
+      words: ["KŪRE", "DRUSTI", "MĪKLA", "SALA"],
+      difficulty: 2,
+    },
+    {
+      category: "UPES AR DZĪVNIEKIEM NOSAUKUMĀ",
+      words: ["STIRNA", "VĒRŠĀDA", "NEVĒŽA", "BRIEDE"],
+      difficulty: 3,
+    },
+    {
+      category: "LIETVĀRDI, KAS MĒDZ BUT ARĪ IZSAUKSMES VĀRDI",
+      words: ["DIEVIŅŠ", "SVIESTS", "NELAIME", "NERAŽA"],
+      difficulty: 4,
+    },
+  ],
+];
